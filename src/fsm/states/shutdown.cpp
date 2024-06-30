f359d5f6dd5f75a3ce6ad3a19e2a0be4a9521d56
kistenklaus <karlsasssie@gmail.com>
2024-06-30 22:53:44 +0200

    shutdown state

--- src/fsm/states/shutdown.cpp
+++ src/fsm/states/shutdown.cpp
@@ -16,9 +16,9 @@ global_state fsm::states::shutdown(global_command cmd, Duration time_since_last_
   // and wait a bit
   if (SHUTDOWN_TIME > time_since_last_transition) {
     if (canzero_get_power_board12_state() != pdu_12v_state_CHANNELS_TELEMETRY 
-          || canzero_get_power_board24_state() != pdu_24v_state_CHANNELS_IDLE) {
+          || canzero_get_power_board24_state() != pdu_24v_state_CHANNELS_OFF) {
       canzero_set_power_board12_command(pdu_12v_command_TELEMETRY);
-      canzero_set_power_board24_command(pdu_24v_command_IDLE);
+      canzero_set_power_board24_command(pdu_24v_command_STOP);
     }
     return global_state_SHUTDOWN;
   }
